14b122e4b187e72939cb7383e73cb3cfb794db82
fGWKZUTL0 <onatik0198@gmail.com>
2023-10-22 04:26:42 +0900

    style/prettierの設定を追加

--- nextdeblog/prettier.config.js
+++ nextdeblog/prettier.config.js
@@ -1,7 +1,8 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  "semi": false,
+  "semi": true,
   "printWidth": 90,
   "tabWidth": 2, // スペースの単位
   "trailingComma": "all",
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
