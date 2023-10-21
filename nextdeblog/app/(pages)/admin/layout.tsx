8b17ce8fca0a5a01ffb0607af25940b87f7d3d55
fGWKZUTL0 <onatik0198@gmail.com>
2023-10-21 17:44:26 +0900

    etc/postgresql作成

--- nextdeblog/app/(pages)/admin/layout.tsx
+++ nextdeblog/app/(pages)/admin/layout.tsx
@@ -1,10 +1,21 @@
+"use client"
+
 import Sidebar from "@/app/components/admin/sidebar/SideBar"
+import { signIn, useSession } from "next-auth/react"
+import { useEffect } from "react"
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { data: session } = useSession()
+  useEffect(() => {
+    if (!session) {
+      signIn();
+    }
+  }, [session]);
+
   return (
     <div className="flex">
       <Sidebar />
--- nextdeblog/app/providers/NextAuthProvider.tsx
+++ nextdeblog/app/providers/NextAuthProvider.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
 const NextAuthProvider = ({ children }: { children: ReactNode }) => {
+  
   return <SessionProvider>{children}</SessionProvider>
 }
 
