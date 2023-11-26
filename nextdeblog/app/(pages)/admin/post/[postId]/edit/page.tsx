b4899ccd03d1071bcf4d25fec2b341f5bf31dc21
fGWKZUTL0 <onatik0198@gmail.com>
2023-11-26 19:52:18 +0900

    feat/next.jsの開発環境いろいろ見直し

--- nextdeblog/app/(pages)/admin/post/[postId]/edit/page.tsx
+++ nextdeblog/app/(pages)/admin/post/[postId]/edit/page.tsx
@@ -1,7 +1,5 @@
 import { getPost } from "@/app/servers/post/getPost"
 import Form from "../../../(form)/Form"
-import { useEffect, useState } from "react"
-import { PostFormType } from "@/app/types/post"
 
 type Props = {
   params: {
