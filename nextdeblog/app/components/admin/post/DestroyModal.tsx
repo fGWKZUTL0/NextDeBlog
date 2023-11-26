626799da1ca067d5e1eba4c405744b0fb1ce1d76
fGWKZUTL0 <onatik0198@gmail.com>
2023-11-26 20:18:51 +0900

    fix/削除ボタンが消えなくなる問題を修正

--- nextdeblog/app/components/admin/post/DestroyModal.tsx
+++ nextdeblog/app/components/admin/post/DestroyModal.tsx
@@ -34,7 +34,9 @@ export default function DestroyModal({post}: DestroyModalProps){
 
   return(
     <>
-      <div className="w-fit h-full bg-orange-700 rounded-full" onClick={() => onOpen()}>
+      <div className="w-fit h-full bg-orange-700 rounded-full" onClick={() =>{ 
+        onOpen()
+      }}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
